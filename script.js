294406e237ad1e2e8be6b4089102acd8ff5e0482
Lily White <lilywhite2005@outlook.com>
2023-03-14 00:23:15 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ fetch("assets/source.txt")
+        .then((resp) => resp.text())
+        .then((data) => data.split('\n')
+                            .sort(() => Math.random() >= 0.5)
+                            .slice(0, 3)
+                            .join('<br/>').replaceAll(' ', '_'))
+        .then((data) => document.getElementById('maindiv').innerHTML = data ) 
+
