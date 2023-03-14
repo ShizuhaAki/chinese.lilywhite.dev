597f500e0feed44877c56d2e4e050771e6036616
Ravenclaw-OIer <44526586+Ravenclaw-OIer@users.noreply.github.com>
2023-03-14 12:00:17 +0800

    fix: JS requires negative value for sort compareFn

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  fetch("assets/source.txt")
         .then((resp) => resp.text())
         .then((data) => data.split('\n')
-                            .sort(() => Math.random() >= 0.5)
+                            .sort(() => Math.random() - 0.5)
                             .slice(0, 3)
                             .join('<br/>').replaceAll(' ', '_'))
         .then((data) => document.getElementById('maindiv').innerHTML = data ) 
